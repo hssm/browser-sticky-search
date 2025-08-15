e38e9912170067100271d160bb182113a322564a
hssm <houssam.salem.au@gmail.com>
2025-08-16 02:34:19 +1000

    Let's try a side-by-side layout. Use same element for consistent style

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # https://github.com/hssm/browser-sticky-search
 # Version 0.1
-
+from PyQt6 import QtWidgets
 from aqt import *
 from anki.hooks import wrap
 from aqt.browser import Browser
@@ -9,18 +9,28 @@ from aqt.browser import Browser
 class BrowserStickySearch:
 
     def browser_will_show(self, browser):
-        sticky = QLineEdit(browser)
-        sticky.setPlaceholderText("Sticky search (type text, then press Enter)")
-        qconnect(sticky.returnPressed, browser.onSearchActivated)
+
+        stickyEdit = QtWidgets.QComboBox(parent=browser)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(9)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(stickyEdit.hasHeightForWidth())
+        stickyEdit.setSizePolicy(sizePolicy)
+        stickyEdit.setEditable(True)
+        stickyEdit.setInsertPolicy(QtWidgets.QComboBox.InsertPolicy.NoInsert)
+        stickyEdit.setObjectName("stickyEdit")
+        stickyEdit.lineEdit().setPlaceholderText("Sticky search (type text, then press Enter")
+        qconnect(stickyEdit.lineEdit().returnPressed, browser.onSearchActivated)
+
 
         # Put ours on top and move existing one down a row
-        browser.form.gridLayout.addWidget(sticky, 0, 1, 1, 1)
-        browser.form.gridLayout.addWidget(browser.form.searchEdit, 1, 1, 1, 1)
+        browser.form.gridLayout.addWidget(stickyEdit, 0, 1, 1, 1)
+        browser.form.gridLayout.addWidget(browser.form.searchEdit, 0, 2, 2, 2)
 
-        self.sticky = sticky
+        self.sticky = stickyEdit
 
     def browser_will_search(self, context):
-        context.search = ' '.join([context.search, self.sticky.text()])
+        context.search = ' '.join([context.search, self.sticky.lineEdit().text()])
         return None
 
     def setup_search(self, *kwargs):
